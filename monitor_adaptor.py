ee46f5281ebe34b4bd1af05b12a9f4e63cbb1f6c
hehaiping <1293460561@qq.com>
2023-02-18 12:00:36 +0800

    refactor

--- monitor_adaptor.py
+++ monitor_adaptor.py
@@ -70,6 +70,7 @@ class MonitorAdaptor:
         Thread(target=self.run, daemon=True, name="monitor_adaptor").start()
 
         self.tracks: Dict[int, List[Track]] = {}
+        self.airplanes: Dict[str, Airplane] = {}
 
     def run(self) -> None:
         loop = asyncio.new_event_loop()
@@ -98,16 +99,55 @@ class MonitorAdaptor:
     def on_message(self, message) -> None:
         print(message)
 
+    def update_center(self, lng: float, lat: float) -> None:
+        message = f"""
+            let parameter = new AMap.LngLat({lng}, {lat});
+            app.$refs.map.map.setCenter(parameter);
+        """
+        self.mq.put(message)
+
+    def update_zooms(self, zoom_min: float, zoom_max: float) -> None:
+        message = f"""
+            let parameter = [{zoom_min}, {zoom_max}];
+            app.$refs.map.map.setZooms(parameter);
+        """
+        self.mq.put(message)
+
+    def update_zoom(self, zoom: float) -> None:
+        message = f"""
+            let parameter = {zoom};
+            app.$refs.map.map.setZoom(parameter);
+        """
+        self.mq.put(message)
+
+    def update_pitch(self, pitch: float) -> None:
+        message = f"""
+            let parameter = {pitch};
+            app.$refs.map.map.setPitch(parameter);
+        """
+        self.mq.put(message)
+
+    def update_limit_bounds(self, south_west_lng: float, south_west_lat: float,
+                         north_east_lng: float, north_east_lat: float) -> None:
+        message = f"""
+            let parameter =  new AMap.Bounds(
+                new AMap.LngLat({south_west_lng}, {south_west_lat}),
+                new AMap.LngLat({north_east_lng}, {north_east_lat})
+            );
+            app.$refs.map.map.setLimitBounds(parameter);
+        """
+        self.mq.put(message)
+
     def update_track(self, tracks: List[Track], clear_timeout=5) -> None:
         for track in tracks:
             if track.id not in self.tracks.keys():
                 self.tracks[track.id] = []
             self.tracks[track.id].append(track)
         
-        now = time()
+        latest_track_at = max(tracks, key=lambda t: t.track_at).track_at
         for id in self.tracks.keys():
             self.tracks[id] = [track for track in self.tracks[id]
-                               if now - track.track_at <= clear_timeout]
+                               if latest_track_at - track.track_at <= clear_timeout]
         self.tracks = {k: v for k, v in self.tracks.items() if len(v) > 0}
 
         def get_parameter() -> str:
@@ -135,61 +175,7 @@ class MonitorAdaptor:
         message = f"""
             let parameter = {get_parameter()};
             let ids = [{",".join([f"'{k}'" for k in self.tracks.keys()])}]
-            app.$refs.map.trackLines.showTracks(parameter, ids);
-        """
-        self.mq.put(message)
-
-    def set_center(self, lng: float, lat: float) -> None:
-        message = f"""
-            let parameter = new AMap.LngLat({lng}, {lat});
-            app.$refs.map.map.setCenter(parameter);
-        """
-        self.mq.put(message)
-
-    def set_zooms(self, zoom_min: float, zoom_max: float) -> None:
-        message = f"""
-            let parameter = [{zoom_min}, {zoom_max}];
-            app.$refs.map.map.setZooms(parameter);
-        """
-        self.mq.put(message)
-
-    def set_zoom(self, zoom: float) -> None:
-        message = f"""
-            let parameter = {zoom};
-            app.$refs.map.map.setZoom(parameter);
-        """
-        self.mq.put(message)
-
-    def set_pitch(self, pitch: float) -> None:
-        message = f"""
-            let parameter = {pitch};
-            app.$refs.map.map.setPitch(parameter);
-        """
-        self.mq.put(message)
-
-    def set_limit_bounds(self, south_west_lng: float, south_west_lat: float,
-                         north_east_lng: float, north_east_lat: float) -> None:
-        message = f"""
-            let parameter =  new AMap.Bounds(
-                new AMap.LngLat({south_west_lng}, {south_west_lat}),
-                new AMap.LngLat({north_east_lng}, {north_east_lat})
-            );
-            app.$refs.map.map.setLimitBounds(parameter);
-        """
-        self.mq.put(message)
-
-    def add_device(self, device: Device) -> None:
-        message = f"""
-            let parameter = {{
-                id: "{device.id}",
-                type: "{device.type}",
-                position: new AMap.LngLat({device.lng}, {device.lat}),
-                extra_info: {{
-                    name: "{device.name}",
-                    functional: {"true" if device.functional else "false"},
-                }}
-            }}
-            app.$refs.map.devices.addDevice(parameter);
+            app.$refs.map.trackLines.updateTracks(parameter, ids);
         """
         self.mq.put(message)
 
@@ -199,7 +185,7 @@ class MonitorAdaptor:
                 id: "{device.id}",
                 type: "{device.type}",
                 position: new AMap.LngLat({device.lng}, {device.lat}),
-                extra_info: {{
+                extraInfo: {{
                     name: "{device.name}",
                     functional: {"true" if device.functional else "false"},
                 }}
@@ -213,29 +199,6 @@ class MonitorAdaptor:
             app.$refs.map.devices.setVisibilityByType(
                 "{type}", {"true" if visibility else "false"}
             );
-        """
-        self.mq.put(message)
-
-    def add_zone(self, zone: Zone) -> None:
-        paths = []
-        for shape in zone.path:
-            path = []
-            for s in shape:
-                path.append(f"new AMap.LngLat({s[0]}, {s[1]})")
-            path = f"[{','.join(path)}]"
-            paths.append(path)
-        paths = f"[{','.join(paths)}]"
-
-        path = ",".join([f"new AMap.LngLat({p[0]}, {p[1]})" for p in zone.path])
-        message = f"""
-            let parameter = {{
-                id: "{zone.id}",
-                type: "{zone.type}",
-                path: {paths},
-                height: {zone.height},
-                color: "{zone.color}",
-            }};
-            app.$refs.map.zones.addZone(parameter);
         """
         self.mq.put(message)
 
@@ -281,7 +244,7 @@ if __name__ == "__main__":
         east = 113.341422
         north = 23.416018
 
-        id = random.randint(1, 1000)
+        id = random.randint(1, 2)
         lng = random.randrange(int(west * 1_000_000), int(east * 1_000_000)) / 1_000_000
         lat = random.randrange(int(south * 1_000_000), int(north * 1_000_000)) / 1_000_000
         alt = random.randint(50, 5000)
@@ -293,23 +256,23 @@ if __name__ == "__main__":
 
     monitor_adaptor = MonitorAdaptor(lambda: print("hi"))
 
-    monitor_adaptor.set_center(113.306646, 23.383048)
+    monitor_adaptor.update_center(113.306646, 23.383048)
     sleep(0.5)
-    monitor_adaptor.set_zooms(8, 16)
+    monitor_adaptor.update_zooms(8, 16)
     sleep(0.5)
-    monitor_adaptor.set_zoom(14)
+    monitor_adaptor.update_zoom(14)
     sleep(0.5)
-    monitor_adaptor.set_pitch(70)
+    monitor_adaptor.update_pitch(70)
     sleep(0.5)
-    monitor_adaptor.set_limit_bounds(113.271213, 23.362449, 113.341422, 23.416018)
+    monitor_adaptor.update_limit_bounds(113.271213, 23.362449, 113.341422, 23.416018)
     sleep(0.5)
-    monitor_adaptor.add_device(Device("1", "horn", 113.306646, 23.383048, "horn1", True))
+    monitor_adaptor.update_device(Device("1", "horn", 113.306646, 23.383048, "horn1", True))
     sleep(2)
     monitor_adaptor.update_device(Device("1", "horn", 113.307646, 23.384048, "horn1", False))
-    # sleep(1)
-    # monitor_adaptor.set_device_visibility_by_type("horn", False)
+    sleep(1)
+    monitor_adaptor.set_device_visibility_by_type("horn", False)
     sleep(0.5)
-    monitor_adaptor.add_zone(Zone("1", "danger",
+    monitor_adaptor.update_zone(Zone("1", "danger",
                                   [[(113.307706,23.3737), (113.315884,23.371746),
                                     (113.314939,23.36729), (113.307043,23.368054)]],
                                   1000, "#0088ffcc"))
@@ -325,6 +288,6 @@ if __name__ == "__main__":
     sleep(0.5)
 
     while True:
-        tracks = [get_random_track() for i in range(3000)]
+        tracks = [get_random_track() for i in range(2)]
         monitor_adaptor.update_track(tracks)
         sleep(1)
